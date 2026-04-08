2e5c970c48e2d9900702683880f9b563b97353e6
AramSeo <102861959+AramSeo@users.noreply.github.com>
2026-04-08 23:29:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE='phuquoc-v15';
-const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='pq-v2';
+const ASSETS=['./index.html','./manifest.json'];
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
   e.respondWith(
     fetch(e.request).then(r=>{
-      const c=r.clone();
-      caches.open(CACHE).then(cache=>cache.put(e.request,c));
+      const clone=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
       return r;
     }).catch(()=>caches.match(e.request))
   );
